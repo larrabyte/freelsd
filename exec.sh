955de4c64903d621b8d729eb342175ba82903477
larrabyte <larrabyte@protonmail.ch>
2020-02-22 18:41:51 +1100

    Create a shell makefile

--- exec.sh
+++ exec.sh
@@ -1,3 +1,58 @@
+# exec.sh
 # made by the larrabyte himself
+# cant make a makefile
 
-qemu-system-i386 -display sdl -kernel bin/bodger.bin
+EXEC="kernel"
+
+# compiler and assembler (for boot.asm)
+CC="i686-elf-gcc"
+AS="nasm"
+
+# flags
+CFLAGS="-Wall -Wextra -O2 -ffreestanding -std=gnu99"
+FFLAGS="-nostdlib"
+AFLAGS="-felf32"
+LFLAGS="-lgcc"
+
+# do not change these :)
+OBJLIST=""
+set -e
+
+function stageone() {
+    for file in src/* ; do
+        if [ -d "$file" ] ; then
+            continue
+        fi
+
+        fbase=$(basename "$file")
+        fname="${fbase%.*}"
+        fext="${fbase##*.}"
+
+        if [ "$fext" == "asm" ] ; then
+            ${AS} ${AFLAGS} ${file} -o obj/${fname}.o
+            printf "Assembling %s...\n" "$file"
+        fi
+
+        if [ "$fext" == "c" ] ; then
+            ${CC} ${CFLAGS} -c ${file} -o obj/${fname}.o
+            printf "Compiling %s...\n" "$file"
+        fi
+    done
+}
+
+function stagetwo() {
+    for file in obj/* ; do
+        OBJLIST="${OBJLIST}${file} "
+    done
+
+    printf "Linking object files...\n"
+    ${CC} -T src/linker.ld ${CFLAGS} ${FFLAGS} ${OBJLIST} -o bin/${EXEC}.bin ${LFLAGS}
+}
+
+# argument parser
+if [ "$1" == "build" ] ; then
+    stageone
+    stagetwo
+fi
+
+qemu-system-i386 -display sdl -kernel bin/kernel.bin
