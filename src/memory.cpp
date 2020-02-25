7ffb72ae853fb380421e8afac1aaba639607dd99
larrabyte <larrabyte@protonmail.ch>
2020-02-25 23:13:40 +1100

    Made memory::copy faster
    Copies 8 bytes at a time rather than 1.

--- src/memory.cpp
+++ src/memory.cpp
@@ -1,17 +1,25 @@
 #include "head/memory.hpp"
+#include <stdint.h>
 
-void memory::set(const void *memory, size_t n, unsigned char value) {
+void memory::set(const void *memory, size_t n, uint8_t value) {
     char *memcharptr = (char*) memory;
     while(n) memcharptr[n--] = value;
 }
 
 void *memory::copy(const void *dest, const void *source, size_t n) {
     if(dest == NULL || source == NULL || dest == source) return NULL;
-    char *sourcecast = (char*) source;
-    char *destcast = (char*) dest;
+    char *src = (char*) source;
+    char *dst = (char*) dest;
+
+    while(n >= sizeof(uint64_t)) {
+        *((uint64_t*) dst) = *((uint64_t*) src);
+        dst += sizeof(uint64_t);
+        src += sizeof(uint64_t);
+        n -= sizeof(uint64_t);
+    }
 
     for(size_t i = 0; i < n; i++) {
-        destcast[i] = sourcecast[i];
+        dst[i] = src[i];
     }
 
     return (void*) dest;
