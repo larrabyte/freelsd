aeefac9ff0bf2eb343f1926a57e50f320640a439
larrabyte <larrabyte@protonmail.ch>
2020-03-21 19:39:43 +1100

    Set memory in 4-byte chunks

--- kernel/arch/i686/memory.cpp
+++ kernel/arch/i686/memory.cpp
@@ -2,8 +2,18 @@
 #include <stdint.h>
 
 void memory::set(const void *memory, uint8_t value, size_t n) {
-    char *memcharptr = (char*) memory;
-    for(size_t i = 0; i < n; i++) memcharptr[i] = value;
+    uint32_t manybits  = (value << 24) | (value << 16) | (value << 8) | value;
+    char *memoryptr = (char*) memory;
+
+    while(n >= sizeof(uint32_t)) {
+        *((uint32_t*) memoryptr) = manybits;
+        memoryptr += sizeof(uint32_t);
+        n -= sizeof(uint32_t);
+    }
+
+    for(size_t i = 0; i < n; i++) {
+        memoryptr[i] = value;
+    }
 }
 
 void *memory::copy(const void *dest, const void *source, size_t n) {
