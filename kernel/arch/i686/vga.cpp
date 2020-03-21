6867226404c70ed3c17063ba9ebcd5eca663a31e
larrabyte <larrabyte@protonmail.ch>
2020-03-21 15:51:40 +1100

    Modify VGA code
    will return without printing if not initialised

--- kernel/arch/i686/vga.cpp
+++ kernel/arch/i686/vga.cpp
@@ -6,10 +6,10 @@
 static const size_t VGA_HEIGHT = 25;
 static const size_t VGA_WIDTH = 80;
 
-uint8_t vga::colour = VGA_LIGHT_GREY | VGA_BLACK << 4;  // Current terminal colours.
-uint16_t *vga::buffer = (uint16_t*) 0xb8000;            // Pointer to video memory.
-size_t vga::column = 0;                                 // Current terminal column.
-size_t vga::row = 0;                                    // Current terminal row.
+uint16_t *vga::buffer = NULL;  // Pointer to video memory.
+size_t vga::column = 0;        // Current terminal column.
+size_t vga::row = 0;           // Current terminal row.
+uint8_t vga::colour;           // Current terminal colours.
 
 static inline uint16_t entry(unsigned char uc, uint8_t colour) {
     return (uint16_t) uc | (uint16_t) colour << 8;
@@ -19,7 +19,19 @@ inline void vga::putentryat(char c, uint8_t colour, size_t x, size_t y) {
     buffer[y * VGA_WIDTH + x] = entry(c, colour);
 }
 
+void vga::setcolour(colour_td fg, colour_td bg) {
+    colour = fg | bg << 4;
+}
+
+void vga::scroll(size_t n) {
+    // Copies ahead of the buffer back into the base pointer, effectively scrolling.
+    memory::copy(buffer, buffer + VGA_WIDTH * n, VGA_WIDTH * VGA_HEIGHT * 2);
+}
+
 void vga::write(const char *data) {
+    // Don't try and print if VGA isn't initialised.
+    if(buffer == NULL) return;
+
     for(size_t i = 0; i < cstr::len(data); i++) {
         // If character is a newline, advance rows.
         if(data[i] == '\n') { column = 0; row++; }
@@ -44,16 +56,8 @@ void vga::write(const char *data) {
     }
 }
 
-void vga::setcolour(colour_td fg, colour_td bg) {
-    colour = fg | bg << 4;
-}
-
-void vga::scroll(size_t n) {
-    // Copies ahead of the buffer back into the base pointer, effectively scrolling.
-    memory::copy(buffer, buffer + VGA_WIDTH * n, VGA_WIDTH * VGA_HEIGHT * 2);
-}
-
 void vga::initialise(void) {
-    // Zero the VGA buffer. Effectively clears the screen.
-    memory::set(buffer, 0, VGA_WIDTH * VGA_HEIGHT * 2);
+    colour = VGA_LIGHT_GREY | VGA_BLACK << 4;            // Set the default colour.
+    buffer = (uint16_t*) 0xb8000;                        // Assign the address of the VGA text buffer. 
+    memory::set(buffer, 0, VGA_WIDTH * VGA_HEIGHT * 2);  // Zero the buffer (clearing the screen).
 }
