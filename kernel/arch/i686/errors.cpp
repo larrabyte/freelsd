76ca0f7a7528e7738056425f3c9c5361d8ac6c52
larrabyte <larrabyte@protonmail.ch>
2020-03-27 11:12:57 +1100

    Add an error frog and a white death screen

---
+++ kernel/arch/i686/errors.cpp
@@ -0,0 +1,29 @@
+#include <gfx/gfx.hpp>
+#include <errors.hpp>
+#include <memory.hpp>
+#include <serial.hpp>
+#include <stdint.h>
+
+__attribute__((noreturn)) void panic(const char *message) {
+    memset(gfx::info.buffer, 0xFF, gfx::info.pixelwidth * gfx::info.pixelheight * gfx::info.bpp);
+
+    // Reset gfx colour and cursor.
+    gfx::colour = 0x00000000;
+    gfx::column = 0;
+    gfx::row = 1;
+
+    gfx::printf(errfrog);
+    gfx::printf("[kernel] FreeLSD panic: %s\n", message);
+    gfx::printf("[kernel] Halting execution.\n");
+
+    while(true) asm volatile("hlt");
+}
+
+extern "C" {
+    // Find out how to randomise this value for security.
+    uintptr_t __stack_chk_guard = 0xDEADBEEF;
+
+    __attribute__((noreturn)) void __stack_chk_fail(void) {
+        panic("Stack smashing guard overwritten.");
+    }
+}
--- kernel/arch/i686/kernel.cpp
+++ kernel/arch/i686/kernel.cpp
@@ -5,10 +5,9 @@
 #include <memory.hpp>
 #include <serial.hpp>
 #include <string.hpp>
+#include <errors.hpp>
 #include <timer.hpp>
 #include <gdt.hpp>
-
-static const char frog[] = "\n  ()-()\n.-(___)-. freelsd development kernel\n _<   >_  beep boop keeping track of time\n \\/   \\/\n\n";
 
 void lighthouse(mb_info_t *mbd) {
     gfx::video_info_t *vinf = gfx::infoptr;
@@ -29,20 +28,22 @@ extern "C" void kernelmain(mb_info_t *mbd, uint32_t magic) {
     kboard::initialise();
 
     // The FreeLSD frog!
-    serial::write(frog);
-    gfx::write(frog);
+    serial::write(stdfrog);
+    gfx::write(stdfrog);
 
     if(checkbit(mbd->flags, 0)) {
         // Read number of memory blocks and divide to retrieve megabytes.
         uint64_t mempages = (mbd->lowermem + mbd->uppermem) / 1024;
 
         // Write available memory to screen.
-        gfx::printf("[kernel] available memory: %dMB\n", mempages);
+        gfx::printf("[kernel] Available memory: %dMB\n", mempages);
     }
 
     // Write VESA video mode information to serial.
-    serial::printf("[kernel] framebuffer address: %p\n", gfx::info.buffer);
-    serial::printf("[kernel] resolution: %dx%dx%d\n", gfx::info.pixelwidth, gfx::info.pixelheight, gfx::info.bpp);
+    serial::printf("[kernel] Framebuffer address: %p\n", gfx::info.buffer);
+    serial::printf("[kernel] Resolution: %dx%dx%d\n", gfx::info.pixelwidth, gfx::info.pixelheight, gfx::info.bpp);
 
+    timer::sleep(1000);
+    panic("what up fools");
     // lighthouse(mbd);
 }
