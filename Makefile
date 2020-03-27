869681e06fdae7ff50f2dcfe52113713e75c8e25
larrabyte <larrabyte@protonmail.ch>
2020-03-27 22:03:26 +1100

    Print serial to console and add uppercase array

--- Makefile
+++ Makefile
@@ -15,7 +15,7 @@ WARNINGS := -Wall -Wextra -Wpedantic -Wno-unused-parameter -Wno-write-strings
 CFLAGS   := $(WARNINGS) -ffreestanding -fstack-protector \
 			-fno-exceptions -fno-rtti -O3 -nostdlib
 
-QFLAGS := -M q35 -display sdl -serial file:serial.log -cdrom build/freelsd.iso
+QFLAGS := -M q35 -display sdl -serial stdio -cdrom build/freelsd.iso
 AFLAGS := -felf32
 
 # -----------------------------
--- kernel/arch/i686/keyboard.cpp
+++ kernel/arch/i686/keyboard.cpp
@@ -1,12 +1,14 @@
 #include <interrupts.hpp>
+#include <multiboot.hpp>
 #include <keyboard.hpp>
 #include <gfx/gfx.hpp>
+#include <serial.hpp>
 #include <hwio.hpp>
 
 namespace kboard {
-    static const char kbamerica[128] = {
-        0,  27, '1', '2', '3', '4', '5', '6', '7', '8',	'9', '0', '-', '=', '\b' /* backspace */,
-        '\t' /* tab */,  'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n' /* enter */,
+    static const char uslayout_lower[128] = {
+        0,  27, '1', '2', '3', '4', '5', '6', '7', '8',	'9', '0', '-', '=', '\b',
+        '\t', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',
         0 /* 29 - control */, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`',
         0/* left shift */, '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 0 /* Right shift */,
         '*', 0 /* alt */, ' ' /* spacebar */, 0 /* capslock */, 0 /* 59 - F1 key ... > */, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -15,15 +17,20 @@ namespace kboard {
         0 /* insert */,	0 /* delete */,	0, 0, 0, 0 /* f11 */, 0 /* f12 */, 0, /* All other keys are undefined */
     };
 
+    static const char uslayout_upper[] = {
+        0, 27, '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 0,
+        0, 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', 0,
+        0, 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '\"', '~', 0,
+        '|', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?'
+    };
+
     void handler(idt::regs32_t *regs) {
         uint8_t scancode = inportb(0x60);
-        char buffer[2];
 
         // Check if bit 7 is set. If not, key has been pressed.
-        if((scancode & 0x80) != 0x80) {
-            buffer[0] = kbamerica[scancode];
-            buffer[1] = '\0';
-            gfx::write(buffer);
+        if(!checkbit(scancode, 7)) {
+            serial::printf("[kboard] scancode (hex %p, dec %d)\n", scancode, scancode);
+            gfx::writechar(uslayout_lower[scancode]);
         }
     }
 
