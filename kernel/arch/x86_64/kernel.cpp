0e8974019eea3fc7b62361e98ec0e38367fcdf9a
larrabyte <larrabyte@protonmail.ch>
2020-06-04 21:08:44 +1000

    Stick 64-bit interrupts on the burner

--- kernel/arch/x86_64/kernel.cpp
+++ kernel/arch/x86_64/kernel.cpp
@@ -1,9 +1,7 @@
-#include <interrupts.hpp>
 #include <serial.hpp>
 #include <stdint.h>
 
 extern "C" void kernelmain(void) {
     serial::initialise();
-    idt::initialise();
     serial::printf("[kernel] hello from long mode!\n");
 }
